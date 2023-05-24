ea1724c381a1f891f52cfe7adff2bd6375f74f97
Wombatoff <mwombatoff@gmail.com>
2023-05-24 18:41:56 +0300

    DEBUG = False

--- beautycity/settings.py
+++ beautycity/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY', 'default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
