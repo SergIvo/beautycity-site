5502b3dd78e3552711ff543eebc456ea2407abee
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-05-26 12:08:57 +0300

    Added applications

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers
 
 from .models import Application
 from phonenumber_field.serializerfields import PhoneNumberField
 
 from django.db import transaction
+
+
+class ApplicationSerializer(ModelSerializer):
+    phonenumber = PhoneNumberField()
+
+    class Meta:
+        model = Application
+        fields = ['name', 'phonenumber', 'question']
+
+    @transaction.atomic
+    def create(self, validated_data):
+        new_application = Application.objects.create(**validated_data)
+        return new_application
